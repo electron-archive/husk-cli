4fb1af3a4cd6f93420c51b5ce68c4faa6ffaa844
Ben Ogle <ogle.ben@gmail.com>
2015-07-26 15:34:38 -0700

    Add some sweet matchers

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -1,1 +1,28 @@
-require("babel/register");
+require("babel/register")
+
+beforeEach(function() {
+  jasmine.addMatchers({
+    toEndWith: function(){
+      return {
+        compare: function(actual, expected) {
+          var pass = actual.endsWith(expected)
+          var notStr = pass ? ' not' : ''
+          var message = "Expected string '#{actualValue}' to#{notStr} end with '#{expectedValue}'"
+          return {pass: pass, message: message}
+        }
+      }
+    },
+
+    toHaveLength: function(){
+      return {
+        compare: function(actual, expectedValue){
+          actualValue = actual.length
+          pass = actualValue === expectedValue
+          notStr = pass ? ' not' : ''
+          message = "Expected array with length #{actualValue} to#{notStr} have length #{expectedValue}"
+          return {pass: pass, message: message}
+        }
+      }
+    }
+  })
+})
