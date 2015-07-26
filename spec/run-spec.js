fb9f6b256e5e451f9f135987d6b3f36ad295f8d7
Ben Ogle <ogle.ben@gmail.com>
2015-07-26 15:34:48 -0700

    Specs for run command

--- spec/run-spec.js
+++ spec/run-spec.js
@@ -1,11 +1,67 @@
+var path = require('path')
 var Utils = require('../src/utils')
 var runCommand = require('../src/husk-cli').runCommand
 
 describe("run command", function(){
-  it("runs the electron app", function(){
+  it("runs the electron app in development mode by default", function(){
     spyOn(Utils, 'spawnCommand')
-    runCommand('run')
+    spyOn(Utils, 'logWarning')
+    spyOn(Utils, 'logError')
+    runCommand('run', {_: ['run']})
 
     expect(Utils.spawnCommand).toHaveBeenCalled()
+
+    let args = Utils.spawnCommand.calls.mostRecent().args
+    args[2]() // the callback
+
+    expect(args[0]).toEndWith(path.join('node_modules', '.bin', 'electron'))
+    expect(args[1][1]).toBe('--environment')
+    expect(args[1][2]).toBe('development')
+
+    expect(Utils.logError).not.toHaveBeenCalled()
+    expect(Utils.logWarning).not.toHaveBeenCalled()
+  })
+
+  it("logs an error when an error happens", function(){
+    spyOn(Utils, 'spawnCommand')
+    spyOn(Utils, 'logWarning')
+    spyOn(Utils, 'logError')
+    runCommand('run', {})
+
+    expect(Utils.spawnCommand).toHaveBeenCalled()
+
+    let args = Utils.spawnCommand.calls.mostRecent().args
+    args[2](new Error) // the callback
+
+    expect(Utils.logError).toHaveBeenCalled()
+    expect(Utils.logWarning).toHaveBeenCalled()
+  })
+
+  it("overrides the environment when an environment is passed in", function(){
+    spyOn(Utils, 'spawnCommand')
+    runCommand('run', {environment: 'production'})
+
+    expect(Utils.spawnCommand).toHaveBeenCalled()
+
+    let args = Utils.spawnCommand.calls.mostRecent().args
+    expect(args[0]).toEndWith(path.join('node_modules', '.bin', 'electron'))
+    expect(args[1][1]).toBe('--environment')
+    expect(args[1][2]).toBe('production')
+    expect(args[1]).toHaveLength(3)
+  })
+
+  it("passes all other cli args through to the electron app", function(){
+    spyOn(Utils, 'spawnCommand')
+    runCommand('run', {_: ['run', 'another'], cats: 'wow'})
+
+    expect(Utils.spawnCommand).toHaveBeenCalled()
+
+    let args = Utils.spawnCommand.calls.mostRecent().args
+    expect(args[0]).toEndWith(path.join('node_modules', '.bin', 'electron'))
+    expect(args[1][1]).toBe('--environment')
+    expect(args[1][2]).toBe('development')
+    expect(args[1][3]).toBe('--cats')
+    expect(args[1][4]).toBe('wow')
+    expect(args[1][5]).toBe('another')
   })
 })
--- src/run.js
+++ src/run.js
@@ -1,11 +1,26 @@
-var path = require('path')
-var Utils = require('./utils')
+let path = require('path')
+let Utils = require('./utils')
+
+let HandledOptions = ['_', 'environment']
 
 module.exports = {
   execute: function(argv) {
     var appPath = Utils.getProjectPath()
     var cmd = path.join(appPath, 'node_modules', '.bin', 'electron')
-    var args = [appPath, '--environment', 'development']
+    var args = [appPath, '--environment', argv.environment || 'development']
+
+    for (var key in argv){
+      if (HandledOptions.indexOf(key) > -1) continue
+      args.push(`--${key}`)
+      args.push(argv[key])
+    }
+
+    if (argv._)
+      for (var value of argv._) {
+        if (value === 'run') continue
+        args.push(value)
+      }
+
     Utils.logVerbose(argv, 'Running', cmd, args)
     Utils.spawnCommand(cmd, args, function(err) {
       if (err){
