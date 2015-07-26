6fb0f30060c279e9316b4ad1f6aaf2c2bffdfe50
Ben Ogle <ogle.ben@gmail.com>
2015-07-25 20:06:21 -0700

    Gruntfile in js

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    babel: {
+      options: {
+        sourceMap: false
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['**/*.js'],
+          dest: "lib",
+          ext: ".js"
+        }]
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+      },
+      all: ['Gruntfile.js', 'src/**/*.js']
+    },
+
+    watch: {
+      scripts: {
+        files: ['src/*.js'],
+        tasks: ['default'],
+        options: {
+          spawn: false
+        },
+      },
+    },
+
+    shell: {
+      test: {
+        command: 'jasmine-focused --captureExceptions --coffee spec/',
+        options:{
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-babel')
+  grunt.loadNpmTasks('grunt-shell')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+
+  grunt.registerTask('clean', function() { require('rimraf').sync('lib') })
+  grunt.registerTask('lint', ['jshint'])
+  grunt.registerTask('default', ['lint', 'babel'])
+  grunt.registerTask('test', ['default', 'shell:test'])
+}
